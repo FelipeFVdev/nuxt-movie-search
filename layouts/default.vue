c745724be13c512c51fb81c54e6721297cfc12d6
FelipeFVdev <feliep8@gmail.com>
2024-12-26 23:10:42 -0300

    feat: add-default-layout-and-index-page

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type MovieHeader from "~/components/MovieHeader.vue";
+</script>
+
+<template>
+  <div>
+    <MovieHeader />
+
+    <section class="flex h-full w-screen">
+      <slot />
+    </section>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <UiContainer class="w-full h-full m-auto pt-20">
+    <p class="text-4xl">Home</p>
+  </UiContainer>
+</template>
+
+<style></style>
