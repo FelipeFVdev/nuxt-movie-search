a1d1464256495f13756697606c313aeaf6529af9
FelipeFVdev <feliep8@gmail.com>
2025-03-06 00:11:14 -0300

    feat: add MovieHeaderMobile and MovieHeaderSearch components

---
+++ components/MovieHeaderMobile.vue
@@ -0,0 +1,67 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const iconTheme = ref("");
+
+onMounted(() => {
+  iconTheme.value =
+    colorMode.value === "dark" ? "lucide:sun-medium" : "lucide:moon";
+});
+
+const toggleIcon = () => {
+  iconTheme.value =
+    iconTheme.value === "lucide:sun-medium"
+      ? "lucide:moon"
+      : "lucide:sun-medium";
+};
+</script>
+
+<template>
+  <div class="flex items-center gap-2 lg:hidden">
+    <UiButton
+      @click="
+        $colorMode.preference = $colorMode.value == 'dark' ? 'light' : 'dark'
+      "
+      size="icon-sm"
+      variant="ghost"
+      tittle="Toggle Theme"
+    >
+      <span class="sr-only">Toggle Theme</span>
+      <Icon :name="iconTheme" @click="toggleIcon" class="h-5 w-5" />
+    </UiButton>
+    <UiSheet>
+      <UiSheetTrigger as-child>
+        <UiButton variant="ghost" size="icon-sm">
+          <Icon name="lucide:menu" class="h-5 w-5" />
+        </UiButton>
+        <UiSheetContent class="w-[90%] p-0">
+          <template #content>
+            <UiSheetTitle class="sr-only" title="Mobile menu" />
+            <UiSheetDescription class="sr-only" description="Mobile menu" />
+            <UiSheetX class="z-20" />
+
+            <UiScrollArea class="h-full p-5">
+              <div class="flex flex-col gap-2">
+                <UiButton variant="ghost" class="justify-start text-base" to="#"
+                  >Home</UiButton
+                >
+
+                <UiButton variant="ghost" class="justify-start text-base" to="#"
+                  >Pricing</UiButton
+                >
+
+                <UiGradientDivider class="my-5" />
+
+                <UiGradientDivider class="my-5" />
+
+                <UiButton to="#">Sign up</UiButton>
+                <UiButton variant="outline" to="#">Log in</UiButton>
+              </div>
+            </UiScrollArea>
+          </template>
+        </UiSheetContent>
+      </UiSheetTrigger>
+    </UiSheet>
+  </div>
+</template>
+
+<style></style>
---
+++ components/MovieHeaderSearch.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+const moviesStore = useMoviesStore();
+const { handleSearch } = moviesStore;
+const { searchString } = storeToRefs(moviesStore);
+</script>
+<template>
+  <div class="flex w-auto items-center px-4">
+    <div class="relative flex items-center">
+      <div
+        class="flex h-10 items-center rounded-md rounded-r-none border border-r-0 border-foreground/5 bg-secondary"
+      >
+        <UiDropdownMenu>
+          <UiDropdownMenuTrigger asChild class="flex items-center">
+            <Button variant="ghost" class="h-8 pr-2 text-center">
+              <span class="ml-4"> All </span>
+              <Icon name="lucide:chevron-down" class="ml-2 size-4" />
+            </Button>
+          </UiDropdownMenuTrigger>
+          <UiDropdownMenuContent align="start">
+            <UiDropdownMenuItem>Titles</UiDropdownMenuItem>
+            <UiDropdownMenuItem>TV Episodes</UiDropdownMenuItem>
+            <UiDropdownMenuItem>Celebs</UiDropdownMenuItem>
+            <UiDropdownMenuItem>Companies</UiDropdownMenuItem>
+            <UiDropdownMenuItem>Keywords</UiDropdownMenuItem>
+          </UiDropdownMenuContent>
+        </UiDropdownMenu>
+      </div>
+      <UiInput
+        v-model="searchString"
+        placeholder="Search for a movie..."
+        class="min-w-[500px] rounded-l-none border-l-0 border-foreground/5 bg-secondary outline-none placeholder:text-muted-foreground hover:bg-foreground/5 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm"
+        @keyup.enter="handleSearch"
+        @change="handleSearch"
+      />
+
+      <Icon name="lucide:search" class="w-4s absolute right-0 mr-4 h-4" />
+    </div>
+  </div>
+</template>
+
+<style></style>
